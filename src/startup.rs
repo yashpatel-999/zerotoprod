97de6272074b8c30efd4ab67bb505c2d68bbc70e
yashpatel-999 <thotayash47@gmail.com>
2025-10-17 16:21:44 +0530

    used env_logger to log every operation in terminal

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -11,6 +12,7 @@ pub fn run(
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())

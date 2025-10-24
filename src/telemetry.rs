908c282bd936d31cc5493428ef6bccfe41a0119f
yashpatel <thotayash47@gmail.com>
2025-10-24 11:36:02 +0530

    removed get_subscriber from telemetry.rs as get_dual_subscriber function meets all the requirements

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -8,26 +8,6 @@ use tracing_appender::{non_blocking,rolling};
 use tracing_subscriber::fmt;
 
 
-pub fn get_subscriber<Sink>(
-    name: String,
-    env_filter: String,
-    sink: Sink,
-) -> impl Subscriber + Send + Sync
-where
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
-    let env_filter=EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer=BunyanFormattingLayer::new(
-        name,
-        sink
-    );
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync + 'static) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");

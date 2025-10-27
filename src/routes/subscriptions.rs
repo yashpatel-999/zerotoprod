f67ff871c9164cdcb9cc1b536dd610191821c28e
yashpatel <thotayash47@gmail.com>
2025-10-27 10:15:25 +0530

    added validation and error handling for name

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,14 +21,18 @@ pub struct FormData {
     ret
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    // Record the form data in the span before consuming it
     tracing::Span::current()
         .record("subscriber_email", tracing::field::display(&form.email))
         .record("subscriber_name", tracing::field::display(&form.name));
 
+    let name=match(SubscriberName::parse(form.0.name)){
+        Ok(name)=>name,
+        Err(_)=>return HttpResponse::BadRequest().finish(),
+    };
+    
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name: SubscriberName::parse(form.0.name).expect("Name Valiation Failed")
     };
     
     match insert_subscriber(&pool, &new_subscriber).await {

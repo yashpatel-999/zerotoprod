892d9ee0014281e25112ce272d5022fea82a3439
yashpatel <thotayash47@gmail.com>
2025-10-28 10:38:26 +0530

    handled returning exact error in case of Invalid name so that user experience will be great

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use secrecy::ExposeSecret;
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> std::io::Result<()>{
     let (subscriber,_guard)=get_dual_subscriber(
         "zerotoprod".into(),
         "info".into(),
@@ -16,9 +16,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "zerotoprod"
     );
     init_subscriber(subscriber);
-    let configuration = get_configuration()?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await?;
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
--- src/routes/domain/subscriber_name.rs
+++ src/routes/domain/subscriber_name.rs
@@ -7,14 +7,19 @@ pub struct SubscriberName(String);
 impl SubscriberName{
     pub fn parse(s:String)->Result<SubscriberName,SubscribeError>{
         let is_empty_or_whitespace = s.trim().is_empty();
+        if is_empty_or_whitespace{
+            return Err(SubscribeError::ValidationError(format!("Invalid subscriber name: 'name should not be empty'")));
+        }
         let is_too_long = s.graphemes(true).count() > 256;
+        if is_too_long{
+            return Err(SubscribeError::ValidationError(format!("Invalid subscriber name: 'name should not be greater than 256 characters'")));
+        }
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            Err(SubscribeError::ValidationError(format!("Invalid subscriber name: '{}'",s)))
-        } else {
-            Ok(Self(s))
+        if contains_forbidden_characters{
+            return Err(SubscribeError::ValidationError(format!("Invalid subscriber name: 'name should not contain '{:?}'",forbidden_characters)));
         }
+        Ok(Self(s))
     }
 }
 impl AsRef<str> for SubscriberName {
